507cf42735a5cf440c5a70749efe3705c07a7551
go2023 <tetyanago2018@gmail.com>
2024-04-03 16:39:28 -0700

    summary component was adjusted

--- src/components/summary/index.jsx
+++ src/components/summary/index.jsx
@@ -1,10 +1,85 @@
 import React from 'react';
+import {Box, Flex, Heading, Text} from "@chakra-ui/react";
 
 const Summary = () => {
     return (
-        <h1>
-            Summary Component
-        </h1>
+        <Box
+            p="6"
+            border={"1px solid"}
+            borderColor={"gray.100"}
+            overflow={"hidden"}
+            borderRadius={"10"}
+            background={"white"}
+            display={"flex"}
+        >
+            <Flex
+                w="full"
+                justifyContent={"center"}
+                alignItems={"center"}
+                flexDirection={{
+                    base: "column",
+                    sm: "column",
+                    md: "column",
+                    lg: "row",
+                    xl: "row",
+                }}
+            >
+                <Flex
+                    flex={1}
+                    w={"full"}
+                    flexDirection={"column"}
+                    alignItems={"center"}
+                    justifyContent={"space-evenly"}
+                    ml={"-20"}
+                    mr={"2"}
+                >
+                    <Heading size={"md"} mb={"4"} color={"gray.600"}>
+                        Balance is 100
+                    </Heading>
+                    <Flex
+                        justifyContent={"space-evenly"}
+                        alignItems={"center"}
+                        bg={"gray.50"}
+                        w="full"
+                        h="100px"
+                        border={"1px solid"}
+                        borderColor={"gray.100"}
+                    >
+                        <Flex flexDirection={"column"}>
+                            <Heading color={"gray.700"}>$ 100</Heading>
+                            <Text color={"gray.600"}>Total Income</Text>
+                        </Flex>
+                    </Flex>
+                    <Flex
+                        justifyContent={"space-evenly"}
+                        alignItems={"center"}
+                        bg={"gray.50"}
+                        w="full"
+                        h="100px"
+                        border={"1px solid"}
+                        borderColor={"gray.100"}
+                    >
+                        <Flex flexDirection={"column"}>
+                            <Heading color={"gray.700"}>$ 100</Heading>
+                            <Text color={"gray.600"}>Total Expense</Text>
+                        </Flex>
+                    </Flex>
+                </Flex>
+                <Box
+                    flex={1}
+                    mt={"10"}
+                    ml={"-90px"}
+                    mr={"5"}
+                    width={"300px"}
+                    height={"300px"}
+                    display={"flex"}
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                >
+                  <Heading>Chart</Heading>
+                </Box>
+            </Flex>
+        </Box>
     );
 };
 
