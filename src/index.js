ecb264b188e1d9c9c4a709fcdf0780e95900e172
go2023 <tetyanago2018@gmail.com>
2024-04-03 15:20:40 -0700

    chakraProvider was implemented

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {ChakraProvider} from "@chakra-ui/react";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
