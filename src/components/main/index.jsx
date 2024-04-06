d70b3c670e1af94a01ae55486fb2b65ac887f24a
go2023 <tetyanago2018@gmail.com>
2024-04-05 21:55:33 -0700

    Final commit

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -57,8 +57,14 @@ const Main = () => {
                 justifyContent={"space-evenly"}
                 flexDirection={["column", "column", "column", "row", "row"]}
             >
-                <ExpenseView />
-                <ExpenseView />
+                <ExpenseView
+                    data={allTransactions.filter((item) => item.type === "expense")}
+                    type={"expense"}
+                />
+                <ExpenseView
+                    data={allTransactions.filter((item) => item.type === "income")}
+                    type={"income"}
+                />
             </Flex>
         </Flex>
     );
